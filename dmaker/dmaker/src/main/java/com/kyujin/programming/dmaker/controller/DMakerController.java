e56d6b2e4d7b801912a1c5a5ad862f3316f3b95b
leekykin <leekujin14@gmail.com>
2022-01-17 17:34:09 +0900

    2022-01-17 Starting DMaker project

---
+++ dmaker/dmaker/src/main/java/com/kyujin/programming/dmaker/controller/DMakerController.java
@@ -0,0 +1,23 @@
+package com.kyujin.programming.dmaker.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@RestController
+public class DMakerController {
+
+    @GetMapping("/developers")
+    public List<String> getAllDevelopers() {
+        // GET /developers HTTP/1.1
+        log.info("GET /developers HTTP/1.1");
+
+        return Arrays.asList("Kyukin", "Jobs", "Bill");
+    }
+
+
+}
