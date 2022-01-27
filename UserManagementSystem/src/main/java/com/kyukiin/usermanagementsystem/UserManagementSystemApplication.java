7a6e33cced9c17115713a0a3aaa5f96694689250
leekyukin <leekujin14@gmail.com>
2022-01-27 13:40:00 +0900

    2022-01-27 Review spring jpa

---
+++ UserManagementSystem/src/main/java/com/kyukiin/usermanagementsystem/UserManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kyukiin.usermanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserManagementSystemApplication.class, args);
+    }
+
+}
---
+++ UserManagementSystem/src/test/java/com/kyukiin/usermanagementsystem/UserManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyukiin.usermanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
