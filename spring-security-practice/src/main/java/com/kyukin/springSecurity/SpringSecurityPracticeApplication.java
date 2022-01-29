202cc1f2320fe477c39313f7f55902ef898e4e19
leekyukin <leekujin14@gmail.com>
2022-01-29 12:15:51 +0900

    2022-01-29 Set up some dependencies

---
+++ spring-security-practice/src/main/java/com/kyukin/springSecurity/SpringSecurityPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kyukin.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPracticeApplication.class, args);
+    }
+
+}
---
+++ spring-security-practice/src/test/java/com/kyukin/springSecurity/SpringSecurityPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyukin.springSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
