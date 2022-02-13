90d68aaacc1a7d8e3798930e306d280ea3c6b7dd
leekyukin <leekujin14@gmail.com>
2022-02-13 18:31:57 +0900

    2022-02-13

--- spring-security-practice/src/main/java/com/kyukin/springSecurity/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kyukin.springSecurity.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String name);
-}
