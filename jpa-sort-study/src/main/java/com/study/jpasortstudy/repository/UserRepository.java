a7b557fbf781043364e092dd49d0cee640ffad3f
leekyukin <leekujin14@gmail.com>
2022-03-26 20:21:06 +0900

    2022-03-26 paging 오류남 ㅣ히히히니뉴ㅠㅠ

--- jpa-sort-study/src/main/java/com/study/jpasortstudy/repository/UserRepository.java
+++ jpa-sort-study/src/main/java/com/study/jpasortstudy/repository/UserRepository.java
@@ -1,23 +1,29 @@
 package com.study.jpasortstudy.repository;
 
 import com.study.jpasortstudy.demain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
     // Last1 이 일반 문자열로 인식되어 findByName 과
     // 같은 쿼리문으로 동작하고, 같은 결과가 나온다.
-    User findLast1ByName(String name);
+    List<User> findLast1ByName(String name);
 
     // Last1 과 같은 의미로 쿼리문을 작석아하면 아래와 같다.
     // 아이디로 내림차순 정령한것의 맨 위 히니
-    User findTop1ByNameOrderByIdDesc(String name);
+    List<User> findTop1ByNameOrderByIdDesc(String name);
 
     // Id 는 내림차순, email 은 오름차순으로 정령한 값의 맨 위 하나
-    User findFirst1ByNameOrderByIdDescEmailAsc(String name);
+    List<User> findFirst1ByNameOrderByIdDescEmailAsc(String name);
 
     // 위와 같은 뜻으로 사용할 수 있지만 훨씬 가독성이 뛰어나고 활용성도 높다.
-    User findFirstByName(String name, Sort sort);
+    List<User> findFirstByName(String name, Sort sort);
+
+    Page<User> findByName(String name, Pageable pageable);
 }
--- jpa-sort-study/src/test/java/com/study/jpasortstudy/repository/SortTest.java
+++ jpa-sort-study/src/test/java/com/study/jpasortstudy/repository/SortTest.java
@@ -1,8 +1,11 @@
 package com.study.jpasortstudy.repository;
 
+import org.hibernate.criterion.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 @SpringBootTest
@@ -14,7 +17,7 @@ class SortTest {
     @Test
     void sort() {
 
-        String name = "moon";
+        String name = "leekujin";
 
         System.out.println("findLast1ByName : " + userRepository.findLast1ByName(name));
 
@@ -24,6 +27,10 @@ class SortTest {
 
         // 위의 쿼리와 같은 뜻이지만 훨씬 간편하고 가독성이 뛰어나고 활용성도 높다.
         System.out.println("findFirstByNameWithSortParams : " +
-                userRepository.findFirstByName("leekujin", Sort.by(Sort.Order.desc("id"), Sort.Order.asc("email"))));
+                userRepository.findFirstByName(name, Sort.by(Sort.Order.desc("id"), Sort.Order.asc("email"))));
+
+        System.out.println("findByNameWithPaging : "
+                + userRepository.findFirstByName(name, PageRequest.of(1, 1, Sort.by(Sort.Order.desc("id")))).getContents()));
+
     }
 }
