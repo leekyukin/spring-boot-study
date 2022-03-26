00fd9985f5d4cbf8744421a12adb7a10659fdf8a
leekyukin <leekujin14@gmail.com>
2022-03-26 19:54:08 +0900

    2022-03-26 jpa qeuryMehtod with sort test

--- jpa-sort-study/src/main/java/com/study/jpasortstudy/repository/UserRepository.java
+++ jpa-sort-study/src/main/java/com/study/jpasortstudy/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.study.jpasortstudy.repository;
 
 import com.study.jpasortstudy.demain.User;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -11,9 +12,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findLast1ByName(String name);
 
     // Last1 과 같은 의미로 쿼리문을 작석아하면 아래와 같다.
-    // 내림차순으로 정령한것의 맨 위
-    User findTop1ByNameOrderByDesc(String name);
+    // 아이디로 내림차순 정령한것의 맨 위 히니
+    User findTop1ByNameOrderByIdDesc(String name);
 
+    // Id 는 내림차순, email 은 오름차순으로 정령한 값의 맨 위 하나
+    User findFirst1ByNameOrderByIdDescEmailAsc(String name);
 
-
+    // 위와 같은 뜻으로 사용할 수 있지만 훨씬 가독성이 뛰어나고 활용성도 높다.
+    User findFirstByName(String name, Sort sort);
 }
--- jpa-sort-study/src/test/java/com/study/jpasortstudy/repository/SortTest.java
+++ jpa-sort-study/src/test/java/com/study/jpasortstudy/repository/SortTest.java
@@ -3,6 +3,7 @@ package com.study.jpasortstudy.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 
 @SpringBootTest
 class SortTest {
@@ -13,6 +14,16 @@ class SortTest {
     @Test
     void sort() {
 
-        
+        String name = "moon";
+
+        System.out.println("findLast1ByName : " + userRepository.findLast1ByName(name));
+
+        System.out.println("findTop1ByNameOrderByIdDesc : " + userRepository.findTop1ByNameOrderByIdDesc(name) );
+
+        System.out.println("findFirst1ByNameOrderByIdDescEmailAsc : " + userRepository.findFirst1ByNameOrderByIdDescEmailAsc(name));
+
+        // 위의 쿼리와 같은 뜻이지만 훨씬 간편하고 가독성이 뛰어나고 활용성도 높다.
+        System.out.println("findFirstByNameWithSortParams : " +
+                userRepository.findFirstByName("leekujin", Sort.by(Sort.Order.desc("id"), Sort.Order.asc("email"))));
     }
 }
