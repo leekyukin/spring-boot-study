167e356d5b06040013d283b987e3aeb4ec7e6f06
leekyukin <leekujin14@gmail.com>
2022-08-17 23:11:23 +0900

    쳥소 쓱싹

--- jpa-practice/src/main/java/com/kujin/japfkpractice/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kujin.japfkpractice.repository;
-
-import com.kujin.japfkpractice.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
