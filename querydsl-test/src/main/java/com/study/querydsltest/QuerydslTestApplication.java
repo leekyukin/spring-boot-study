78297691ce893918978a83b9fdfc8c9b7946d1b1
leekyukin <leekujin14@gmail.com>
2022-08-25 17:32:55 +0900

    SET :: Initial

---
+++ querydsl-test/src/main/java/com/study/querydsltest/QuerydslTestApplication.java
@@ -0,0 +1,13 @@
+package com.study.querydsltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslTestApplication.class, args);
+    }
+
+}
---
+++ querydsl-test/src/test/java/com/study/querydsltest/QuerydslTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.querydsltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
