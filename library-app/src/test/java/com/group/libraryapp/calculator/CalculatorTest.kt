0bf0e1bd64dfcdd0729597cce535866092d3deaa
leekyukin <leekujin14@gmail.com>
2022-09-07 18:46:21 +0900

    TEST :: Kotlin Calculator

--- library-app/src/test/java/com/group/libraryapp/calculator/CalculatorTest.kt
+++ library-app/src/test/java/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -2,6 +2,7 @@ package com.group.libraryapp.calculator
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.nio.channels.IllegalChannelGroupException
 
 class CalculatorTest {
 
@@ -31,7 +32,6 @@ class CalculatorTest {
         }
     }
 
-    @Test
     fun multiply() {
         // given
         var calculator = Calculator(5);
@@ -45,5 +45,35 @@ class CalculatorTest {
         }
     }
 
+    fun divideTest() {
+        // given
+        var calculator = Calculator(5);
 
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideExceptionError() {
+        // given
+        var calculator = Calculator(5);
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("메세지가 다릅니다.")
+            }
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+
+        throw IllegalStateException("기대하는 예외 발생하지 않음")
+    }
 }
---
+++ library-app/src/test/java/com/group/libraryapp/calculator/JunitCalculatorTest.kt
@@ -0,0 +1,69 @@
+package com.group.libraryapp.calculator
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class JunitCalculatorTest {
+
+    @Test
+    fun addTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.add(5)
+
+        // then
+        Assertions.assertEquals(10, calculator.number)
+    }
+
+    @Test
+    fun minusTest() {
+        // given
+        var calculator = Calculator(5);
+
+        // when
+        calculator.minus(3)
+
+        // then
+        Assertions.assertEquals(2, calculator.number)
+    }
+
+    @Test
+    fun multiply() {
+        // given
+        var calculator = Calculator(5);
+
+        // when
+        calculator.multiply(10)
+
+        // then
+        Assertions.assertEquals(50, calculator.number)
+    }
+
+    @Test
+    fun divideTest() {
+        // given
+        var calculator = Calculator(5);
+
+        // when
+        calculator.divide(2)
+
+        // then
+        Assertions.assertEquals(2, calculator.number)
+    }
+
+    @Test
+    fun divideExceptionError() {
+        // given
+        var calculator = Calculator(5);
+
+        // when
+        var message = assertThrows<IllegalArgumentException> {
+            calculator.divide(0)
+        }.apply {
+            Assertions.assertEquals("0으로 나눌 수 없습니다.", message)
+        }
+    }
+}
