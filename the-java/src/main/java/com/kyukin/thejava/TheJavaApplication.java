179bd8d6426968f85610b549a709d87fbe98d655
leekyukin <leekujin14@gmail.com>
2022-10-14 15:01:59 +0900

    SET :: initial

---
+++ the-java/src/main/java/com/kyukin/thejava/TheJavaApplication.java
@@ -0,0 +1,13 @@
+package com.kyukin.thejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheJavaApplication.class, args);
+    }
+
+}
---
+++ the-java/src/test/java/com/kyukin/thejava/TheJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyukin.thejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
